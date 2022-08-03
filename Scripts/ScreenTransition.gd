9a424c88ff27b19cfb86bdeeec4d1f2b47a8efae
Scyonix <scyonix17@gmail.com>
2022-08-03 13:45:26 -0700

    Update ScreenTransition.gd

--- Scripts/ScreenTransition.gd
+++ Scripts/ScreenTransition.gd
@@ -4,6 +4,6 @@ onready var anim = get_node("AnimationPlayer")
 
 func change_scene(target_scene:String):
 	anim.play("Dissolve")
-	yield(anim,"animation_finished")
+	yield(anim,"animation_finished") #pauses till animaition has finished
 	get_tree().change_scene(target_scene)
 	anim.play_backwards("Dissolve")
