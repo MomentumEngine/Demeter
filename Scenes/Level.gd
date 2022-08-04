ef9087e581d81fd8c46d4a2bf84465857f48ed10
PixelPotato <pixelpotato.6791@outlook.com>
2022-08-04 20:27:34 +0100

    minor changes

---
+++ Scenes/Level.gd
@@ -0,0 +1,10 @@
+extends TileMap
+class_name InteractiveTilemap
+
+export(Dictionary) var TILE_SCENES := {
+	1: preload("res://Tile.tscn")
+}
+
+func _ready():
+	pass # Replace with function body.
+
